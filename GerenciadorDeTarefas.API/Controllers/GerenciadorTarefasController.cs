c7fd5b3c4ff0f537ed4ad212c64e55f4bf0613d2
ronaldmamani <ronaldmamani140@gmail.com>
2025-05-26 23:18:14 -0300

    Adicionar arquivos de projeto.

---
+++ GerenciadorDeTarefas.API/Controllers/GerenciadorTarefasController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorDeTarefas.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class GerenciadorTarefasController : ControllerBase
+{
+
+}
